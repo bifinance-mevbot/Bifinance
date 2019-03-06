ac879df1b3e9b817d9f0dd94535bcbd5f0cafd97
John <john.hadikusumo@gmail.com>
2019-03-06 16:39:00 +1100

    First Push

---
+++ Bifinance/Controllers/Api/arraycalcController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bifinance.Controllers.Api
+{
+    [AllowAnonymous]
+    public class ArrayCalcController : Controller
+    {
+        [Route("API/ARRAYCALC/REVERSE")]
+        [HttpGet]
+        public List<int> Reverse([FromQuery] List<int> productIds)
+        {
+            List<int> results = new List<int>() { };
+            for(int i= productIds.Count-1; i >=0 ; i--)
+            {
+                results.Add(productIds[i]);
+            }
+            return results;
+        }
+
+        [Route("API/ARRAYCALC/DELETEPART")]
+        [HttpGet]
+        public List<int> DeletePart([FromQuery]int position,[FromQuery] List<int> productIds)
+        {
+            List<int> results = new List<int>() { };
+            for (int i = 0; i < productIds.Count ; i++)
+            {
+                if(i != (position-1)) results.Add(productIds[i]);
+            }
+            return results;
+        }
+    }
+}
